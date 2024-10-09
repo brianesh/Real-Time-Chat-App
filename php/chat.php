ab4d867dbbfee0168780419e101ae3c3ff9b7c87
brianesh <yobrasilva@gmail.com>
2024-10-09 21:43:46 +0300

    first commit

--- php/chat.php
+++ php/chat.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 if (!isset($_SESSION['username'])) {
-    header('Location: index.php'); // Redirect to login if not logged in
+    header('Location: index.php'); 
     exit();
 }
 ?>
