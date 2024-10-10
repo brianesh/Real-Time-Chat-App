45a85bf035c4f9631f61f5c8e518c90e1b7dd4e9
brianesh <yobrasilva@gmail.com>
2024-10-10 23:47:04 +0300

    first commit

--- php/login.php
+++ php/login.php
@@ -17,7 +17,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
   
         if (password_verify($password, $hashedPassword)) {
-            // Start session and store user data
+            
             session_start();
             $_SESSION['user_id'] = $id;
             $_SESSION['username'] = $username;
