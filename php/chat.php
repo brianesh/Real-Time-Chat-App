8ae0b032870319f5d0d085b63bec7036e9ddd4ea
brianesh <yobrasilva@gmail.com>
2024-11-19 20:25:13 +0300

    first commit

--- php/chat.php
+++ php/chat.php
@@ -22,7 +22,7 @@ if (!isset($_SESSION['username'])) {
             <p>User1: Hello!</p>
             <p>User2: Hi there!</p>
         </div>
-
+        
         <form id="chatForm" method="POST">
     <input type="text" id="messageInput" name="message" placeholder="Type your message..." required>
     <button type="submit">Send</button>
